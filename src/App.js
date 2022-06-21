d4e2ab18802bbc8e91de422ae5f5f3c1142594ec
kamal <kamalc218@gmail.com>
2022-06-21 17:51:05 +0530

    hi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,133 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+
+const questions = [
+  {
+    questionText: "Qual o idiomafalado no Brasil?",
+    answerOptions: [
+      { answerText: "Português", isCorrect: true },
+      { answerText: "Inglês", isCorrect: false },
+      { answerText: "Francês", isCorrect: false },
+      { answerText: "Alemão", isCorrect: false },
+    ],
+  },
+  {
+    questionText:
+      "Quais os países que têm a maior e a menor expectativa de vida do mundo?",
+    answerOptions: [
+      { answerText: "Japão e Serra Leoa", isCorrect: true },
+      { answerText: "Austrália e Afeganistã", isCorrect: false },
+      { answerText: "Itália e Chade", isCorrect: false },
+      { answerText: "Brasil e Congo", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "Qual empresa criou o Iphone?",
+    answerOptions: [
+      { answerText: "Apple", isCorrect: true },
+      { answerText: "Intel", isCorrect: false },
+      { answerText: "Amazon", isCorrect: false },
+      { answerText: "Microsoft", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "Como aprender a programar?",
+    answerOptions: [
+      { answerText: "Praticando o que se aprende", isCorrect: true },
+      { answerText: "Vendo vídeo", isCorrect: false },
+      { answerText: "Lendo", isCorrect: false },
+      { answerText: "Dormindo", isCorrect: false },
+    ],
+  },
+];
 
 function App() {
+  const [showScore, setShowScore] = useState(false);
+  const [score, setScore] = useState(0);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [correct, setCorrect] = useState(0);
+  const [wrong, setWrong] = useState(0);
+  const [value, setValue] = useState(0);
+
+  function handleAnswer(isCorrect) {
+    if (isCorrect) {
+      setScore(score + 1);
+      setCorrect(correct + 1);
+      setValue(value + 5);
+    }
+    else {
+      setWrong(wrong + 1);
+      setValue(value - 4);
+    }
+
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setShowScore(true);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="upperhead" >
+        <div className="ShowPoint">
+          Point:{value}
+        </div>
+
+        <div className="headsection">
+          <div className="ProgressBar">
+
+            Right Ans
+            <span className="total_progress">
+              <div className="Progress" style={{ width: `${(correct / 4) * 100}%`, height: 15, backgroundColor: 'green' }}>
+
+              </div>
+            </span>
+          </div>
+
+          <div className="ProgressBar">
+
+            Wrong Ans
+            <span className="total_progress">
+              <div className="Progress" style={{ width: `${(wrong / 4) * 100}%`, height: 15, backgroundColor: 'red' }}>
+
+              </div>
+            </span>
+          </div>
+
+        </div>
+
+      </div>
+      {showScore ? (
+        <div className="score-section">
+          Você pontuou {score} de {questions.length}
+        </div>
+      ) : (
+        <>
+          <div className="question-section">
+            <div className="question-count">
+              <span>Question:  {currentQuestion + 1}</span>/{questions.length}
+            </div>
+            <div className="question-text">
+              {questions[currentQuestion].questionText}
+            </div>
+          </div>
+
+          <div className="answer-section">
+            {questions[currentQuestion].answerOptions.map(
+              (answerOption, index) => (
+                <button
+                  onClick={() => handleAnswer(answerOption.isCorrect)}
+                  key={index}
+                >
+                  {answerOption.answerText}
+                </button>
+              )
+            )}
+          </div>
+        </>
+      )}
     </div>
   );
 }
